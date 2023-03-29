c0f8ad38c037e33c5bd75b2812d813c724553a88
Alex Beltran <alexbeltran@berkeley.edu>
2023-03-29 02:32:29 -0700

    Add subaligner and mkvmerge calls

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import os
+
 from flask import Flask, request
-import requests
 import json
 import subprocess
+from pathlib import Path
+import shutil
 
 app = Flask(__name__)
+
 
 @app.route('/align', methods=['POST'])
 def login():
@@ -11,6 +15,52 @@ def login():
         data = json.loads(request.data)
         media = data.get('media')
         subtitle = data.get('subtitle')
+        media_posix = Path(media)
 
-        
-        
+        temp_path = media_posix.parents[0] / Path('temp' + media_posix.suffix)
+        media_path = f"'{media}'"
+        subtitle_path = f"'{subtitle}'"
+        single_aligned_path = f"""'{subtitle + ".aligned"}'"""
+        dual_aligned_path = f"""'{subtitle + ".aligned_dual"}'"""
+
+        if subprocess.run(["subaligner",
+                           "-m",
+                           "single",
+                           "-v",
+                           media_path,
+                           "-s",
+                           subtitle_path,
+                           "-o",
+                           single_aligned_path]):
+
+            if subprocess.run(["subaligner",
+                               "-m",
+                               "dual",
+                               "-v",
+                               media_path,
+                               "-s",
+                               subtitle_path,
+                               "-o",
+                               dual_aligned_path]):
+
+                if subprocess.run(["mkvmerge",
+                                   "-o",
+                                   temp_path,
+                                   media_path,
+                                   "--language",
+                                   "0:eng",
+                                   "--track-name",
+                                   "0:Aligned-Single",
+                                   single_aligned_path,
+                                   "--language",
+                                   "1:eng",
+                                   "--track-name",
+                                   "1:Aligned-Dual",
+                                   dual_aligned_path
+                                   ]):
+                    print('here')
+                    # shutil.move(temp_path, media_path)
+                    # os.remove(single_aligned_path)
+                    # os.remove(dual_aligned_path)
+
+        return 'Success', 200
