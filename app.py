6fe59d6484d064878c72e776444b6fc549a526ac
Alex Beltran <alexbeltran@berkeley.edu>
2023-04-04 23:07:26 -0700

    Add docstrings, type hints, and comments

--- app.py
+++ app.py
@@ -10,14 +10,18 @@ from utils import subprocess_call, sub_call, cleanup_files
 
 app = Flask(__name__)
 
-timeout = os.environ.get('TIMEOUT', 1000)
-
+# Check if alignment jobs will be queued or not
 if os.environ.get('REDIS_HOST') and os.environ.get('REDIS_PORT'):
     from rq import Queue
     from redis import Redis
 
+    # Fetch redis host and port
     redis_host = os.environ.get('REDIS_HOST')
     redis_port = os.environ.get('REDIS_PORT')
+
+    # Set default timeout for rq alignment jobs
+    timeout = os.environ.get('TIMEOUT', 1000)
+    # Subscribe to the "subtitles" queue
     q = Queue('subtitles', connection=Redis(host=redis_host, port=redis_port), default_timeout=timeout)
 else:
     q = None
@@ -25,6 +29,13 @@ else:
 
 @app.route('/align', methods=['POST'])
 def login():
+    """
+    This endpoint attempts both single and dual stage alignment. Jobs are queued or not depending on whether
+    a redis host and port were specified.
+
+    Only works for mp4 and mkv formats. Untested on others.
+    @return:
+    """
     if request.method == 'POST':
         data = json.loads(request.data)
         media_path = data.get('media')
@@ -35,21 +46,29 @@ def login():
                 and media_path is not None:
             media_posix = Path(media_path)
             subtitle_posix = Path(subtitle_path)
-            temp_media_path = media_posix.parents[0] / Path('temp' + media_posix.suffix)
-            temp_subtitle_path = subtitle_posix.parents[0] / Path('temp' + subtitle_posix.suffix)
+            temp_media_path = str(media_posix.parents[0] / Path('temp' + media_posix.suffix))
+            temp_subtitle_path = str(subtitle_posix.parents[0] / Path('temp' + subtitle_posix.suffix))
 
             sub_format = str(subtitle_posix.suffix)
 
             single_aligned_path = f"""{subtitle_path.replace(sub_format, ".aligned" + sub_format)}"""
             dual_aligned_path = f"""{subtitle_path.replace(sub_format, ".aligned_dual" + sub_format)}"""
 
+            # Create temp subtitle file
             shutil.copy(subtitle_path, temp_subtitle_path)
+            # Call ffprobe to determin audio/video/subtitle track IDs
             data = subprocess.run(['ffprobe', '-loglevel', 'error', '-show_streams', '-of', 'json', media_path],
                                   capture_output=True).stdout
             d = json.loads(data)['streams']
+            # Identify audio channel IDs
             audio_inds = [i for i, x in enumerate(d) if x['codec_type'] == 'audio']
+
+            # Identify existing subtitle tracks to be cleaved when embedding newly aligned subtitles
+            # It is assumed that existing subtitle tracks are from a previous call of this endpoint.
+            # If they exist then the subtitle file must have been upgraded in/by bazarr
             sub_inds = ",".join([str(i) for i, x in enumerate(d) if x['codec_type'] == 'subtitle'])
 
+            # Identify what is presumed to be the only english audio track. (other langs may exist)
             langs = [None] * len(audio_inds)
             channel = '0'
             if len(audio_inds) > 1:
@@ -63,6 +82,7 @@ def login():
                     if lang == 'eng':
                         channel = str(ind)
 
+            # Define single alignment command
             single = ["subaligner",
                       "-m",
                       "single",
@@ -75,6 +95,7 @@ def login():
                       "-o",
                       single_aligned_path]
 
+            # Define dual alignment command
             dual = ["subaligner",
                     "-m",
                     "dual",
@@ -87,12 +108,13 @@ def login():
                     "-o",
                     dual_aligned_path]
 
+            # If existing subtitle tracks are detected, remove them
             if len(sub_inds) > 0:
                 sub = ["mkvmerge",
                        "-o",
                        temp_media_path,
-                       "-s",
-                       "!" + sub_inds,
+                       "-s",  # The -s option allows us to handle existing subtitles
+                       "!" + sub_inds,  # The ! operator lets us specify which to exclude
                        media_path,
                        "--language",
                        "0:eng",
@@ -105,6 +127,7 @@ def login():
                        "0:Aligned-Dual",
                        dual_aligned_path
                        ]
+            # Else only add the newly aligned subtitles
             else:
                 sub = ["mkvmerge",
                        "-o",
@@ -122,14 +145,17 @@ def login():
                        dual_aligned_path
                        ]
             if q:
+                # Chain alignment jobs together
                 job1 = q.enqueue(subprocess_call, kwargs={"command": single})
                 job2 = q.enqueue(subprocess_call, kwargs={"command": dual}, depends_on=job1)
+                # Embed successfully aligned subtitles
                 job3 = q.enqueue(sub_call,
                                  kwargs={"command": sub,
                                          "single_path": single_aligned_path,
                                          "dual_path": dual_aligned_path},
                                  depends_on=job2)
 
+                # Cleanup all temporary files
                 q.enqueue(cleanup_files,
                           kwargs={"temp_media_path": temp_media_path,
                                   "media_path": media_path,
@@ -138,24 +164,28 @@ def login():
                                   "temp_subtitle_path": temp_subtitle_path},
                           depends_on=job3)
             else:
+                # Keep a list of subtitle embed arguments to remove
                 drop_index = []
                 try:
                     subprocess_call(single)
                 except subprocess.CalledProcessError:
+                    # If single stage alignment fails, remove corresponding args in function call
                     drop_index += [-6, -7, -8, -9, -10]
                 try:
                     subprocess_call(dual)
                 except subprocess.CalledProcessError:
+                    # If dual stage alignment fails, remove corresponding args in function call
                     drop_index += [-1, -2, -3, -4, -5]
-
+                # Delete bad args
                 for ind in drop_index:
                     del sub[ind]
 
+                # Call the modified subtitle embed command
                 try:
                     subprocess_call(sub)
                 except subprocess.CalledProcessError:
                     pass
-
+                # Cleanup al temporary files
                 cleanup_files(temp_media_path, media_path, single_aligned_path, dual_aligned_path, temp_subtitle_path)
 
             return 'Success', 200
--- utils.py
+++ utils.py
@@ -3,7 +3,20 @@ import shutil
 import subprocess
 
 
-def cleanup_files(temp_media_path, media_path, single_aligned_path, dual_aligned_path, temp_subtitle_path):
+def cleanup_files(temp_media_path: str,
+                  media_path: str,
+                  single_aligned_path: str,
+                  dual_aligned_path: str,
+                  temp_subtitle_path: str) -> None:
+    """
+    This function attempts to delete all temporary filese creating during subtitle alignment
+
+    @param temp_media_path: Temporary media file to be deleted (.mp4 or .mkv)
+    @param media_path: Original media path to be overwritten (.mp4 or .mkv)
+    @param single_aligned_path: Path to single stage aligned subtitle
+    @param dual_aligned_path: Path to dual stage aligned subtitle
+    @param temp_subtitle_path: Temporary subtitle file to be deleted
+    """
     try:
         shutil.move(temp_media_path, media_path)
     except:
@@ -21,11 +34,23 @@ def cleanup_files(temp_media_path, media_path, single_aligned_path, dual_aligned
     except:
         pass
 
-def subprocess_call(command):
+
+def subprocess_call(command: list) -> None:
+    """
+    Wrapper function on subprocess.run for rq worker compatibility
+    @param command: Subprocess compatible function call as a list. e.g. ["ls", "-a"]
+    """
     subprocess.run(command, check=True)
 
 
-def sub_call(command, single_path, dual_path):
+def sub_call(command: list, single_path: str, dual_path: str) -> None:
+    """
+    This function determines which stages of subtitle alignment succeeded and embeds
+    those that were successful into the original media file
+    @param command: Command to embed aligned subtitles into media file
+    @param single_path: Path to single stage aligned subtitle
+    @param dual_path:  Path to dual stage aligned subtitle
+    """
     drop_index = []
     if not os.path.exists(single_path):
         drop_index += [-6, -7, -8, -9, -10]
